5d8b37c0eec32642b6a13db36cc7b83f23676941
Dane Artis <daneartis4@gmail.com>
2023-03-16 11:55:38 -0600

    Add some project/environment files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='smartdisplay',
+    version='1.0',
+    packages=['display', '_deprecated', 'external_api'],
+    url='https://github.com/Dartis4/smartdisplay',
+    license='MIT',
+    author='Dane Artis',
+    author_email='daneartis4@gmail.com',
+    description='A simple Raspberry Pi smart display'
+)
